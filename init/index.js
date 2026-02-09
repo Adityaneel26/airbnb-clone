83bb45954bd7e57510bf4081446a80358fcb54f7
Aditya <adityaneel555@gmail.com>
2026-02-09 20:02:47 +0530

    change in init file for image

--- init/index.js
+++ init/index.js
@@ -14,7 +14,9 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data=initData.data.map((obj)=>({...obj,owner:"697eef11075a9e75d18868b0"}))
   await Listing.insertMany(initData.data);
+
   console.log("data was initialized");
 };
 
