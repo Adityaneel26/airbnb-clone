e9a7fdbe89511382db07f750475c4fb63ab53b9f
Aditya Neel <adityaneel555@gmail.com>
2026-02-24 08:03:38 +0530

    Fix image object formatting in data.js

--- init/data.js
+++ init/data.js
@@ -3,7 +3,7 @@ const sampleListings = [
     title: "Cozy Beachfront Cottage",
     description:
       "Escape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
-    image:{
+    image:{ 
       filename:"beachfront-cottage.jpg",
       url:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
 
